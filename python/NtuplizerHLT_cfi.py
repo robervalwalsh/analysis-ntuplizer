d598ea0128086473e80c0a7c4151d34828537b90
Roberval Walsh <roberval.walsh@gmail.com>
2017-12-19 09:27:28 +0100

    fragment of ntuplizer config to be used when running HLT

---
+++ python/NtuplizerHLT_cfi.py
@@ -0,0 +1,37 @@
+import FWCore.ParameterSet.Config as cms
+
+TFileService = cms.Service("TFileService",
+	fileName = cms.string('ntuple.root')
+)
+
+from Analysis.Ntuplizer.MssmHbbNtuplizerTriggerPaths_cfi import *
+from Analysis.Ntuplizer.MssmHbbNtuplizerL1Seeds_cfi import *
+from Analysis.Ntuplizer.MssmHbbNtuplizerBtag_cfi import *
+from Analysis.Ntuplizer.MssmHbbNtuplizerTriggerEvent_cfi import *
+
+
+## ============  THE NTUPLIZER!!!  ===============
+ntuplizer           = cms.EDAnalyzer("Ntuplizer",
+    # Imported settings (always at the beginning)
+    MssmHbbNtuplizerBtag,
+    MssmHbbNtuplizerTriggerPaths,
+    MssmHbbNtuplizerL1Seeds,
+    MssmHbbNtuplizerTriggerEvent,
+    MonteCarlo      = cms.bool(False),
+    ## Monte Carlo only
+#     GenFilterInfo   = cms.InputTag("genFilterEfficiencyProducer"),
+#     GenRunInfo      = cms.InputTag("generator"),
+#     GenEventInfo    = cms.InputTag("generator"),
+#     PileupInfo      = cms.InputTag("addPileupInfo","","HLT"),
+    ###################
+    TotalEvents     = cms.InputTag ('TotalEvents'),
+    FilteredEvents  = cms.InputTag ('FilteredEvents'),
+    PatJets         = cms.VInputTag( cms.InputTag('slimmedJets'), cms.InputTag('slimmedJetsPuppi') ),
+#    FixedGridRhoAll = cms.InputTag ('fixedGridRhoAll'),
+    PatMuons        = cms.VInputTag(cms.InputTag('slimmedMuons') ), 
+    PrimaryVertices = cms.VInputTag(cms.InputTag('offlineSlimmedPrimaryVertices') ),
+    TriggerResults  = cms.VInputTag(cms.InputTag('TriggerResults','','HLT') ),
+    L1TJets         = cms.VInputTag(cms.InputTag('caloStage2Digis','Jet','RECO'), ),
+    L1TMuons        = cms.VInputTag(cms.InputTag('gmtStage2Digis','Muon','RECO'), ),
+    TriggerEvent    = cms.VInputTag(cms.InputTag('hltTriggerSummaryAOD','','HLT'), ),
+)
