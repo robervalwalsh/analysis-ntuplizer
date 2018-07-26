330237c8d68b803b77c79d5bb201997f06055ed8
Roberval Walsh <roberval.walsh@gmail.com>
2018-07-26 15:06:47 +0200

    configuration running the b-jet energy regression

---
+++ test/ntuplizer_94X_mc_2017_fall17-v3.py
@@ -0,0 +1,278 @@
+# For the ntuple production
+import FWCore.ParameterSet.Config as cms
+
+from Configuration.StandardSequences.Eras import eras
+
+process = cms.Process('MssmHbb',eras.Run2_2017,eras.run2_nanoAOD_94XMiniAODv2)
+
+process.load('FWCore.MessageService.MessageLogger_cfi')
+process.MessageLogger.cerr.FwkReport.reportEvery = cms.untracked.int32(100000)
+
+##  Using MINIAOD. GlobalTag just in case jet re-clustering, L1 trigger filter  etc is needed to be done
+process.load('Configuration.StandardSequences.MagneticField_AutoFromDBCurrent_cff')
+process.load('Configuration.Geometry.GeometryRecoDB_cff')
+process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_cff')
+from Configuration.AlCa.GlobalTag import GlobalTag
+process.GlobalTag = GlobalTag(process.GlobalTag, '94X_mc2017_realistic_v14')
+
+process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(100) )
+
+## TFileService
+output_file = 'ntuple.root'
+process.TFileService = cms.Service('TFileService',
+   fileName = cms.string(output_file)
+)
+
+from Analysis.Ntuplizer.run2017.MssmHbbTriggerResultsFilter_cfi import *
+
+## ============ TRIGGER FILTER =============== 
+## Enable below at cms.Path if needed - DATA ONLY!!!
+process.triggerSelection = cms.EDFilter( 'TriggerResultsFilter',
+    MssmHbbTriggerResultsFilter,
+    hltResults = cms.InputTag( 'TriggerResults', '', 'HLT' ),
+    l1tResults = cms.InputTag( '' ),
+    l1tIgnoreMask = cms.bool( False ),
+    l1techIgnorePrescales = cms.bool( False ),
+    daqPartitions = cms.uint32( 1 ),
+    throw = cms.bool( False )
+)
+
+
+# only need if discriminator is new or recommended not for JEC
+bTagDiscriminators = [
+    'pfCombinedInclusiveSecondaryVertexV2BJetTags',
+    'pfJetProbabilityBJetTags',
+    'pfDeepCSVJetTags:probudsg',
+    'pfDeepCSVJetTags:probb',
+    'pfDeepCSVJetTags:probc',
+    'pfDeepCSVJetTags:probbb',
+    'pfDeepCSVJetTags:probcc',
+]
+
+### ==== Regression ==== ####
+process.bJetVars = cms.EDProducer("JetRegressionVarProducer",
+    pvsrc = cms.InputTag("offlineSlimmedPrimaryVertices"),
+    src = cms.InputTag("slimmedJets"),    
+    svsrc = cms.InputTag("slimmedSecondaryVertices"),
+    gpsrc = cms.InputTag("prunedGenParticles"),
+    #musrc = cms.InputTag("slimmedMuons"),
+    #elesrc = cms.InputTag("slimmedElectrons")
+)
+
+process.slimmedJetsWithUserData = cms.EDProducer("PATJetUserDataEmbedder",
+     src = cms.InputTag("slimmedJets"),
+     userFloats = cms.PSet(
+         leadTrackPt = cms.InputTag("bJetVars:leadTrackPt"),
+         leptonPtRel = cms.InputTag("bJetVars:leptonPtRel"),
+         leptonPtRatio = cms.InputTag("bJetVars:leptonPtRatio"),
+         leptonPtRelInv = cms.InputTag("bJetVars:leptonPtRelInv"),
+         leptonPtRelv0 = cms.InputTag("bJetVars:leptonPtRelv0"),
+         leptonPtRatiov0 = cms.InputTag("bJetVars:leptonPtRatiov0"),
+         leptonPtRelInvv0 = cms.InputTag("bJetVars:leptonPtRelInvv0"),
+         leptonDeltaR = cms.InputTag("bJetVars:leptonDeltaR"),
+         leptonPt = cms.InputTag("bJetVars:leptonPt"),
+         vtxPt = cms.InputTag("bJetVars:vtxPt"),
+         vtxMass = cms.InputTag("bJetVars:vtxMass"),
+         vtx3dL = cms.InputTag("bJetVars:vtx3dL"),
+         vtx3deL = cms.InputTag("bJetVars:vtx3deL"),
+         ptD = cms.InputTag("bJetVars:ptD"),
+         genPtwNu = cms.InputTag("bJetVars:genPtwNu"),
+         
+         ),
+     userInts = cms.PSet(
+        vtxNtrk = cms.InputTag("bJetVars:vtxNtrk"),
+        leptonPdgId = cms.InputTag("bJetVars:leptonPdgId"),
+     ),
+)
+
+process.bjetNN= cms.EDProducer("BJetEnergyRegressionMVA",
+    backend = cms.string("TF"),
+    src = cms.InputTag("slimmedJetsWithUserData"),
+    pvsrc = cms.InputTag("offlineSlimmedPrimaryVertices"),
+    svsrc = cms.InputTag("slimmedSecondaryVertices"),
+    rhosrc = cms.InputTag("fixedGridRhoFastjetAll"),
+
+    weightFile =  cms.FileInPath("PhysicsTools/NanoAOD/data/breg_training_2017.pb"),
+    name = cms.string("JetRegNN"),
+    isClassifier = cms.bool(False),
+    variablesOrder = cms.vstring(["Jet_pt","Jet_eta","rho","Jet_mt","Jet_leadTrackPt","Jet_leptonPtRel","Jet_leptonDeltaR","Jet_neHEF","Jet_neEmEF","Jet_vtxPt","Jet_vtxMass","Jet_vtx3dL","Jet_vtxNtrk","Jet_vtx3deL","Jet_numDaughters_pt03","Jet_energyRing_dR0_em_Jet_rawEnergy","Jet_energyRing_dR1_em_Jet_rawEnergy","Jet_energyRing_dR2_em_Jet_rawEnergy","Jet_energyRing_dR3_em_Jet_rawEnergy","Jet_energyRing_dR4_em_Jet_rawEnergy","Jet_energyRing_dR0_neut_Jet_rawEnergy","Jet_energyRing_dR1_neut_Jet_rawEnergy","Jet_energyRing_dR2_neut_Jet_rawEnergy","Jet_energyRing_dR3_neut_Jet_rawEnergy","Jet_energyRing_dR4_neut_Jet_rawEnergy","Jet_energyRing_dR0_ch_Jet_rawEnergy","Jet_energyRing_dR1_ch_Jet_rawEnergy","Jet_energyRing_dR2_ch_Jet_rawEnergy","Jet_energyRing_dR3_ch_Jet_rawEnergy","Jet_energyRing_dR4_ch_Jet_rawEnergy","Jet_energyRing_dR0_mu_Jet_rawEnergy","Jet_energyRing_dR1_mu_Jet_rawEnergy","Jet_energyRing_dR2_mu_Jet_rawEnergy","Jet_energyRing_dR3_mu_Jet_rawEnergy","Jet_energyRing_dR4_mu_Jet_rawEnergy","Jet_chHEF","Jet_chEmEF","Jet_leptonPtRelInv","isEle","isMu","isOther","Jet_mass","Jet_ptd"]),
+    variables = cms.PSet(
+    Jet_pt = cms.string("pt*jecFactor('Uncorrected')"),
+    Jet_mt = cms.string("mt*jecFactor('Uncorrected')"),
+    Jet_eta = cms.string("eta"),
+    Jet_mass = cms.string("mass*jecFactor('Uncorrected')"),
+    Jet_ptd = cms.string("userFloat('ptD')"),
+    Jet_leadTrackPt = cms.string("userFloat('leadTrackPt')"),
+    Jet_vtxNtrk = cms.string("userInt('vtxNtrk')"),
+    Jet_vtxMass = cms.string("userFloat('vtxMass')"),
+    Jet_vtx3dL = cms.string("userFloat('vtx3dL')"),
+    Jet_vtx3deL = cms.string("userFloat('vtx3deL')"),
+    Jet_vtxPt = cms.string("userFloat('vtxPt')"),
+    #Jet_leptonPt = cms.string("userFloat('leptonPt')"),
+    Jet_leptonPtRel = cms.string("userFloat('leptonPtRelv0')"),
+    Jet_leptonPtRelInv = cms.string("userFloat('leptonPtRelInvv0')*jecFactor('Uncorrected')"),
+    Jet_leptonDeltaR = cms.string("userFloat('leptonDeltaR')"),
+    #Jet_leptonPdgId = cms.string("userInt('leptonPdgId')"),
+    Jet_neHEF = cms.string("neutralHadronEnergyFraction()"),
+    Jet_neEmEF = cms.string("neutralEmEnergyFraction()"),
+    Jet_chHEF = cms.string("chargedHadronEnergyFraction()"),
+    Jet_chEmEF = cms.string("chargedEmEnergyFraction()"),
+    isMu = cms.string("?abs(userInt('leptonPdgId'))==13?1:0"),
+    isEle = cms.string("?abs(userInt('leptonPdgId'))==11?1:0"),
+    isOther = cms.string("?userInt('leptonPdgId')==0?1:0"),
+    ),
+     inputTensorName = cms.string("ffwd_inp"),
+     outputTensorName = cms.string("ffwd_out/BiasAdd"),
+     outputNames = cms.vstring(["corr","res"]),
+     outputFormulas = cms.vstring(["at(0)*0.28492164611816406+1.0596693754196167","0.5*(at(2)-at(1))*0.28492164611816406"]),
+     nThreads = cms.uint32(1),
+     singleThreadPool = cms.string("no_threads"),
+)
+
+process.slimmedJetsWithUserDataWithReg = cms.EDProducer("PATJetUserDataEmbedder",
+     src = cms.InputTag("slimmedJetsWithUserData"),
+     userFloats = cms.PSet(
+         bJetRegCorr = cms.InputTag("bjetNN:corr"),
+         bJetRegRes = cms.InputTag("bjetNN:res"),
+         ),
+)
+
+
+### ==== Updated Jets ==== ###
+from PhysicsTools.PatAlgos.tools.jetTools import *
+
+
+# Puppi specifics
+from PhysicsTools.PatAlgos.tools.helpers import getPatAlgosToolsTask
+patAlgosToolsTask = getPatAlgosToolsTask(process)
+from PhysicsTools.PatAlgos.patPuppiJetSpecificProducer_cfi import patPuppiJetSpecificProducer
+process.patPuppiJetSpecificProducer = patPuppiJetSpecificProducer.clone(
+   src=cms.InputTag("slimmedJetsPuppi"),
+)
+patAlgosToolsTask.add(process.patPuppiJetSpecificProducer)
+updateJetCollection(
+   process,
+   labelName = 'PuppiJetSpecific',
+   jetSource = cms.InputTag('slimmedJetsPuppi'),
+)
+process.updatedPatJetsPuppiJetSpecific.userData.userFloats.src = ['patPuppiJetSpecificProducer:puppiMultiplicity', 'patPuppiJetSpecificProducer:neutralPuppiMultiplicity', 'patPuppiJetSpecificProducer:neutralHadronPuppiMultiplicity', 'patPuppiJetSpecificProducer:photonPuppiMultiplicity', 'patPuppiJetSpecificProducer:HFHadronPuppiMultiplicity', 'patPuppiJetSpecificProducer:HFEMPuppiMultiplicity' ]
+
+
+
+### ============ Jet energy correctiosn update ============== (not really running!???)
+## Update the slimmedJets in miniAOD: corrections from the chosen Global Tag are applied and the b-tag discriminators are re-evaluated
+updateJetCollection(
+    process,
+    jetSource = cms.InputTag('slimmedJetsWithUserDataWithReg'),
+    jetCorrections = ('AK4PFchs', cms.vstring(['L1FastJet', 'L2Relative', 'L3Absolute']), 'None'),
+#    btagDiscriminators = bTagDiscriminators
+)
+updateJetCollection(
+    process,
+    labelName = 'Puppi',
+    jetSource = cms.InputTag('updatedPatJetsPuppiJetSpecific'),
+    jetCorrections = ('AK4PFPuppi', cms.vstring(['L1FastJet', 'L2Relative', 'L3Absolute']), 'None'),
+#    btagDiscriminators = bTagDiscriminators
+)
+updateJetCollection(
+    process,
+    labelName = 'AK8',
+    jetSource = cms.InputTag('slimmedJetsAK8'),
+    jetCorrections = ('AK8PFchs', cms.vstring(['L1FastJet', 'L2Relative', 'L3Absolute']), 'None'),
+#    btagDiscriminators = bTagDiscriminators    
+)
+
+
+## ============ EVENT FILTER COUNTER ===============
+## Filter counter (maybe more useful for MC)
+process.TotalEvents    = cms.EDProducer('EventCountProducer')
+process.FilteredEvents = cms.EDProducer('EventCountProducer')
+
+## ============ PRIMARY VERTEX FILTER ===============
+process.primaryVertexFilter = cms.EDFilter('VertexSelector',
+   src = cms.InputTag('offlineSlimmedPrimaryVertices'), # primary vertex collection name
+   cut = cms.string('!isFake && ndof > 4 && abs(z) <= 24 && position.Rho <= 2'), # ndof>thr=4 corresponds to sum(track_weigths) > (thr+3)/2 = 3.5 so typically 4 good tracks
+   filter = cms.bool(True),   # otherwise it won't filter the events, just produce an empty vertex collection.
+)
+
+from Analysis.Ntuplizer.run2017.MssmHbbNtuplizerTriggerPaths_cfi import *
+from Analysis.Ntuplizer.run2017.MssmHbbNtuplizerL1Seeds_cfi import *
+from Analysis.Ntuplizer.run2017.MssmHbbNtuplizerTriggerObjects_cfi import *
+from Analysis.Ntuplizer.run2017.MssmHbbNtuplizerBtag_cfi import *
+
+
+## ============  THE NTUPLIZER!!!  ===============
+process.MssmHbb     = cms.EDAnalyzer('Ntuplizer',
+    # Imported settings (always at the beginning)
+    MssmHbbNtuplizerBtag,
+    MssmHbbNtuplizerTriggerPaths,
+    MssmHbbNtuplizerL1Seeds,
+    MssmHbbNtuplizerTriggerObjects,
+    
+    MonteCarlo      = cms.bool(True),
+    ## Monte Carlo only
+    CrossSection    = cms.double(1.),  # in pb
+    GenFilterInfo   = cms.InputTag("genFilterEfficiencyProducer"),
+    GenRunInfo      = cms.InputTag("generator"),
+    GenEventInfo    = cms.InputTag("generator"),
+    GenJets         = cms.VInputTag(cms.InputTag("slimmedGenJets")),
+    GenParticles    = cms.VInputTag(cms.InputTag("prunedGenParticles")),
+    PileupInfo      = cms.InputTag("slimmedAddPileupInfo"),    
+    ###################
+    TotalEvents     = cms.InputTag ('TotalEvents'),
+    FilteredEvents  = cms.InputTag ('FilteredEvents'),
+#    PatJets         = cms.VInputTag( cms.InputTag('slimmedJets'), cms.InputTag('slimmedJetsPuppi'), cms.InputTag('slimmedJetsAK8'), ),
+    PatJets         = cms.VInputTag( cms.InputTag('updatedPatJets'), cms.InputTag('updatedPatJetsPuppi'), cms.InputTag('updatedPatJetsAK8'), ),
+    JECRecords      = cms.vstring  (              'AK4PFchs',                     'AK4PFPuppi',                        'AK8PFchs', ), # for the JEC uncertainties
+    JERRecords      = cms.vstring  (              'AK4PFchs',                     'AK4PFPuppi',                        'AK8PFchs', ), # for the JER uncertainties
+    FixedGridRhoAll = cms.InputTag ('fixedGridRhoAll'),
+    PatMuons        = cms.VInputTag(cms.InputTag('slimmedMuons') ), 
+    PrimaryVertices = cms.VInputTag(cms.InputTag('offlineSlimmedPrimaryVertices') ),
+    TriggerResults  = cms.VInputTag(cms.InputTag('TriggerResults','','HLT') ),
+    L1TJets         = cms.VInputTag(cms.InputTag('caloStage2Digis','Jet','RECO'), ),
+    L1TMuons        = cms.VInputTag(cms.InputTag('gmtStage2Digis','Muon','RECO'), ),
+    TriggerObjectStandAlone = cms.VInputTag(cms.InputTag('slimmedPatTrigger'), ),
+
+)
+
+process.p = cms.Path(
+          process.TotalEvents *
+#          process.triggerSelection *
+          process.primaryVertexFilter *
+          process.FilteredEvents *
+          process.bJetVars * process.slimmedJetsWithUserData * process.bjetNN * process.slimmedJetsWithUserDataWithReg *
+          process.patJetCorrFactors * process.updatedPatJets *
+          process.patPuppiJetSpecificProducer * process.updatedPatJetsPuppiJetSpecific *
+          process.patJetCorrFactorsPuppi * process.updatedPatJetsPuppi *
+          process.patJetCorrFactorsAK8 * process.updatedPatJetsAK8 *
+          process.MssmHbb
+            )
+
+
+readFiles = cms.untracked.vstring()
+secFiles = cms.untracked.vstring() 
+process.source = cms.Source ('PoolSource',fileNames = readFiles, secondaryFileNames = secFiles)
+readFiles.extend( [
+#   '/store/mc/RunIIFall17MiniAOD/SUSYGluGluToBBHToBB_M-350_TuneCP5_13TeV-pythia8/MINIAODSIM/94X_mc2017_realistic_v11-v1/00000/02D5DD76-241B-E811-A02E-0CC47A2B0214.root',
+   '/store/mc/RunIIFall17MiniAODv2/SUSYGluGluToBBHToBB_NarrowWidth_M-350_TuneCP5_13TeV-pythia8/MINIAODSIM/PU2017_12Apr2018_94X_mc2017_realistic_v14-v1/20000/5CCCC2D7-BB41-E811-8AD3-FA163EF96190.root',
+] );
+
+
+secFiles.extend( [
+       ] )
+
+# ============ Output MiniAOD ======================
+# process.out = cms.OutputModule("PoolOutputModule",
+#                                fileName = cms.untracked.string('patTuple.root'),
+#                                outputCommands = cms.untracked.vstring('keep *' )
+#                                )
+# process.outpath = cms.EndPath(process.out)
+
+# ## ============ JSON Certified data ===============   BE CAREFUL!!!
+# ## Don't use with CRAB!!!
+# import FWCore.PythonUtilities.LumiList as LumiList
+# import FWCore.ParameterSet.Types as CfgTypes
+# process.source.lumisToProcess = CfgTypes.untracked(CfgTypes.VLuminosityBlockRange())
+# JSONfile = 'json.txt'
+# myLumis = LumiList.LumiList(filename = JSONfile).getCMSSWString().split(',')
+# process.source.lumisToProcess.extend(myLumis)
