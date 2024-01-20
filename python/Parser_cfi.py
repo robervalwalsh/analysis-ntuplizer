858a10e30d09e9465c7fae4b5b8822fad1553759
Roberval Walsh <roberval.walsh@gmail.com>
2024-01-20 13:33:18 +0100

    improvements

--- python/Parser_cfi.py
+++ python/Parser_cfi.py
@@ -7,95 +7,80 @@ import FWCore.ParameterSet.VarParsing as VarParsing
 
 def parser(yml_file=None):
 
-   cmssw_base = os.getenv("CMSSW_BASE")
+   cmssw_base = os.getenv("CMSSW_BASE")+'/'
    
    # command line options parsing
    options = VarParsing.VarParsing()
 
    options.register('maxEvents',
-                    100,
-                    VarParsing.VarParsing.multiplicity.singleton,
-                    VarParsing.VarParsing.varType.int,
-                    "maximum number of events")
+                  100,
+                  VarParsing.VarParsing.multiplicity.singleton,
+                  VarParsing.VarParsing.varType.int,
+                  "maximum number of events")
 
    options.register('globalTag',
-                    '',
-                    VarParsing.VarParsing.multiplicity.singleton,
-                    VarParsing.VarParsing.varType.string,
-                    "condition global tag for the job (\"auto:run2_data\" is default)")
-                    
+                  '130X_dataRun3_PromptAnalysis_v1',
+                  VarParsing.VarParsing.multiplicity.singleton,
+                  VarParsing.VarParsing.varType.string,
+                  "condition global tag for the job (\"130X_dataRun3_PromptAnalysis_v1\" is default)")
+                  
    options.register('year',
-                    2017,
-                    VarParsing.VarParsing.multiplicity.singleton,
-                    VarParsing.VarParsing.varType.int,
-                    "year of data taking")
+                  2023,
+                  VarParsing.VarParsing.multiplicity.singleton,
+                  VarParsing.VarParsing.varType.int,
+                  "year of data taking")
 
    options.register('type',
-                    'data',
-                    VarParsing.VarParsing.multiplicity.singleton,
-                    VarParsing.VarParsing.varType.string,
-                    "data or mc")
+                  'data',
+                  VarParsing.VarParsing.multiplicity.singleton,
+                  VarParsing.VarParsing.varType.string,
+                  "data or mc")
 
    options.register('xsection',
-                    -1.,
-                    VarParsing.VarParsing.multiplicity.singleton,
-                    VarParsing.VarParsing.varType.float,
-                    "MC cross section")
+                  -1.,
+                  VarParsing.VarParsing.multiplicity.singleton,
+                  VarParsing.VarParsing.varType.float,
+                  "MC cross section")
 
    options.register('triggerInfo',
-                    '',
-                    VarParsing.VarParsing.multiplicity.singleton,
-                    VarParsing.VarParsing.varType.string,
-                    "Trigger info")
+                  '',
+                  VarParsing.VarParsing.multiplicity.singleton,
+                  VarParsing.VarParsing.varType.string,
+                  "Trigger info")
 
    options.register('outputFile',
-                    "ntuple.root",
-                    VarParsing.VarParsing.multiplicity.singleton,
-                    VarParsing.VarParsing.varType.string,
-                    "name for the output root file (\"ntuple.root\" is default)")
+                  "mssmhbb_ntuple.root",
+                  VarParsing.VarParsing.multiplicity.singleton,
+                  VarParsing.VarParsing.varType.string,
+                  "name for the output root file (\"ntuple.root\" is default)")
 
    options.register('inputFiles',
-                    '',
-                    VarParsing.VarParsing.multiplicity.list,
-                    VarParsing.VarParsing.varType.string,
-                    "files to process")
+                  '/store/data/Run2023D/JetMET0/MINIAOD/22Sep2023_v2-v1/2540000/012f384f-443c-4ba1-af3e-86d5ef25afc8.root',
+                  VarParsing.VarParsing.multiplicity.list,
+                  VarParsing.VarParsing.varType.string,
+                  "files to process")
 
    options.register('json',
-                    '',
-                    VarParsing.VarParsing.multiplicity.singleton,
-                    VarParsing.VarParsing.varType.string,
-                    "JSON file (do not use with CRAB!)")
+                  '/eos/user/c/cmsdqm/www/CAF/certification/Collisions23/Cert_Collisions2023_366442_370790_Golden.json',
+                  VarParsing.VarParsing.multiplicity.singleton,
+                  VarParsing.VarParsing.varType.string,
+                  "JSON file (do not use with CRAB!)")
 
    options.register('version',
-                    '',
+                    '1',
                     VarParsing.VarParsing.multiplicity.singleton,
                     VarParsing.VarParsing.varType.int,
                     "ntuple production version")
 
    options.parseArguments()
-   
-   ## default_info.yml
-   input_files = []
-   global_tag = ''
-   def_info = cmssw_base+'/src/Analysis/Ntuplizer/data/ntuples/'+str(options.year)+'/v'+str(options.version)+'/default_info.yml'
-   with open(def_info) as f:
-      def_info_data = yaml.safe_load(f)
-   for dttype,info in def_info_data.items():
-      if dttype != options.type:
-         continue
-      input_files = info['input_files']
-      global_tag = info['global_tag'][0]
-   trigger_info=cmssw_base+'/src/Analysis/Ntuplizer/data/ntuples/'+str(options.year)+'/v'+str(options.version)+'/trigger_info.yml'  
-   
-   # set defaults
-   if not options.inputFiles:
-      options.setDefault('inputFiles',input_files)
-   if not options.globalTag:
-      options.setDefault('globalTag',global_tag)
-   if not options.triggerInfo:
-      options.setDefault('triggerInfo',trigger_info)
 
-
+   trigger_info = ''
+   trigger_base_path = '' 
+   if options.version:
+      # use the trigger from analysis-ntuples
+      trigger_info='src/Analysis/Ntuplizer/data/ntuples/'+str(options.year)+'/v'+str(options.version)+'/trigger_info.yml'  
+      trigger_base_path = "$CMSSW_BASE/"
+   
    ##
    print('Python Configuration Options')
    print('----------------------------')
@@ -103,7 +88,7 @@ def parser(yml_file=None):
    print("year              : ", options.year)
    print("type              : ", options.type)
    print("globalTag         : ", options.globalTag)
-   print("triggerInfo       : ", options.triggerInfo)
+   print("triggerInfo       : ", trigger_base_path+trigger_info)
    print("inputFiles        : ", options.inputFiles)
    print("outputFile        : ", options.outputFile)
    print("maxEvents         : ", options.maxEvents)
@@ -114,6 +99,8 @@ def parser(yml_file=None):
    print('----------------------------')
    print
 
-
+   if trigger_info:
+      trigger_info=cmssw_base+trigger_info
+      options.setDefault('triggerInfo',trigger_info)
    
    return options
